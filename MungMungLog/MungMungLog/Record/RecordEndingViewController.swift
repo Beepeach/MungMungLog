50fa6f73949dc2cc5dee5c7623d21da9cd0ac9f9
JOJUNHEE <eh7457303j@gmail.com>
2020-12-01 22:09:11 +0900

    RecordEndingViewController class 생성

---
+++ MungMungLog/MungMungLog/Record/RecordEndingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecordEndingViewController.swift
+//  MungMungLog
+//
+//  Created by JunHee Jo on 2020/12/01.
+//
+
+import UIKit
+
+class RecordEndingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
