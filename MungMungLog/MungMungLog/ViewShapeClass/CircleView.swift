a69728fd8a266e338a7946b9ea7dc7e037e3096c
JOJUNHEE <eh7457303j@gmail.com>
2020-12-07 23:41:06 +0900

    산책시작, 완료버튼 Dynamic Type Font 지원
    글씨 크기에 맞춰 버튼의 크기가 조절되도록 변경 최소 버튼의 크기를 조정하고 큰 버튼의 경우 원이 아닌 rounded가 된다.

--- MungMungLog/MungMungLog/ViewShapeClass/CircleView.swift
+++ MungMungLog/MungMungLog/ViewShapeClass/CircleView.swift
@@ -10,7 +10,7 @@ import UIKit
 class CircleView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
-        layer.cornerRadius = self.frame.height / 2
+        layer.cornerRadius = self.frame.width / 2
         clipsToBounds = true
     }
     
