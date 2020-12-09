8e56452f2fd43e39eeab887b6c303ef4244062e9
JOJUNHEE <eh7457303j@gmail.com>
2020-12-09 11:15:34 +0900

    산책 기록화면 UI배치 및 RecordEditingViewController.swift 파일 생성
    아직 테이블뷰로 구현할지 스크롤뷰로 구현 할지 고민중 이여서 배치만 해놓은 상태
    RecordEditingViewController class를 scene과 연결

---
+++ MungMungLog/MungMungLog/Record/RecordEditingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecodEditingViewController.swift
+//  MungMungLog
+//
+//  Created by JunHee Jo on 2020/12/09.
+//
+
+import UIKit
+
+class RecordEditingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
