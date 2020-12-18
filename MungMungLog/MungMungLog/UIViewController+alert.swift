6a599249e2e4fd18583f8da7d774ce6ec1a7f7ec
JOJUNHEE <eh7457303j@gmail.com>
2020-12-19 02:30:51 +0900

    presentTwoButtonAlert 메서드 수정 및 기록중지 화면과 산책후기록 연결
    handler를 다루기 위해 presentTwoButtonAlert메서드에 handler parameter 추가.
    alert에서 화면전환을 하기위해서 WalkRecordEditingViewControler의 storyboardID를 walkRecordEditingViewController로 지정 및 handler 구현

--- MungMungLog/MungMungLog/UIViewController+alert.swift
+++ MungMungLog/MungMungLog/UIViewController+alert.swift
@@ -11,9 +11,9 @@ extension UIViewController {
         present(oneButtonAlert, animated: true, completion: nil)
     }
     
-    func presentTwoButtonAlert(alertTitle: String, message: String, confirmActionTitle: String, cancelActionTitle: String) {
+    func presentTwoButtonAlert(alertTitle: String, message: String, confirmActionTitle: String, cancelActionTitle: String, handler: ((UIAlertAction) -> Void)? = nil) {
         let twoButtonAlert = UIAlertController(title: alertTitle, message: message, preferredStyle: .alert)
-        let confirmAlertAction = UIAlertAction(title: confirmActionTitle, style: .destructive, handler: nil)
+        let confirmAlertAction = UIAlertAction(title: confirmActionTitle, style: .destructive, handler: handler)
         let cancelAlertAction = UIAlertAction(title: cancelActionTitle, style: .default, handler: nil)
     
         twoButtonAlert.addAction(confirmAlertAction)
--- MungMungLog/MungMungLog/Walk/WalkRecordEndingViewController.swift
+++ MungMungLog/MungMungLog/Walk/WalkRecordEndingViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 class WalkRecordEndingViewController: UIViewController {
 
     @IBAction func stopRecord(_ sender: Any) {
-        presentTwoButtonAlert(alertTitle: "산책을 마치셨나요?", message: "지금까지의 기록을 저장하시려면 저장을 눌러주세요.", confirmActionTitle: "저장", cancelActionTitle: "취소")
+        presentTwoButtonAlert(alertTitle: "산책을 마치셨나요?", message: "지금까지의 기록을 저장하시려면 저장을 눌러주세요.", confirmActionTitle: "저장", cancelActionTitle: "취소") {_ in
+            guard let editingViewController = self.storyboard?.instantiateViewController(withIdentifier: "walkRecordEditingViewController") else { return }
+            
+            editingViewController.modalPresentationStyle = .fullScreen
+            self.present(editingViewController, animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad() {
