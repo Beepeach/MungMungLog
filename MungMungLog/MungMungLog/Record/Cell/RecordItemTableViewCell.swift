aa343241bef14190fde69f5256f8d3ac3b1280cc
JOJUNHEE <eh7457303j@gmail.com>
2020-12-19 23:39:13 +0900

    recordItemContentView 이름 수정 및 아이콘 경고 수정 및 산책 후 기록화면 DatePicker 수정
    이름이 모호하여 recordItemContentView -> recordItemContainerView로 수정
    iOS 13.0 부터 사용 가능한 임시 아이콘 삭제.
    iOS 14.0 부터 사용 가능한 Date Picker 수정.

--- MungMungLog/MungMungLog/Record/Cell/RecordItemTableViewCell.swift
+++ MungMungLog/MungMungLog/Record/Cell/RecordItemTableViewCell.swift
@@ -9,20 +9,20 @@ import UIKit
 
 class RecordItemTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var recordItemContentView: UIView!
+    @IBOutlet weak var recordItemContainerView: UIView!
     @IBOutlet weak var recordItemImageView: UIImageView!
     @IBOutlet weak var recordItemTitleLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        recordItemContentView.layer.cornerRadius = 10
+        recordItemContainerView.layer.cornerRadius = 10
         if #available(iOS 13.0, *) {
-            recordItemContentView.layer.borderColor = UIColor.systemGray5.cgColor
+            recordItemContainerView.layer.borderColor = UIColor.systemGray5.cgColor
         } else {
             // Fallback on earlier versions
         }
-        recordItemContentView.layer.borderWidth = 2
+        recordItemContainerView.layer.borderWidth = 2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
