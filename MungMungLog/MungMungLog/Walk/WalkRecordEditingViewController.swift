d88badc8893d186073652abf688caf1f078dfb2e
JOJUNHEE <eh7457303j@gmail.com>
2021-01-01 00:48:15 +0900

    산책 기록 화면에서 앨범과 카메라에서 사진 한 장 선택 가능하도록 구현
    사진 권한을 묻기 위해 Privacy - Camera Usage Description, Privacy - Photo Library Usage Description 추가.
    walkRecordImageView에 image를 넣기위해 UIImagePickerControllerDelegate, UINavigationControllerDelegate 추가 및 코드 구현.

--- MungMungLog/MungMungLog/Walk/WalkRecordEditingViewController.swift
+++ MungMungLog/MungMungLog/Walk/WalkRecordEditingViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 class WalkRecordEditingViewController: UIViewController {
     
     var walkRecordTime: Int = 0
+    let imagePicker = UIImagePickerController()
     
+    @IBOutlet weak var walkRecordImageView: UIImageView!
     @IBOutlet weak var walkDateAndTimeLabel: UILabel!
     @IBOutlet weak var walkRecordTimeLabel: UILabel!
     
@@ -38,8 +40,60 @@ class WalkRecordEditingViewController: UIViewController {
         // 데이터베이스에 산책데이터를 저장하는 코드
     }
     
+    
+    // ToDo
+    // 일단 사진 1개만 추가 가능하도록 구현.
+    // 추후에 여러 사진 선택가능하도록
+    @IBAction func selectPhoto(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "카메라", style: .default) {_ in
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                presentCamera()
+            } else {
+                self.presentOneButtonAlert(alertTitle: "알림",
+                                           message: "카메라 사용이 불가능합니다.",
+                                           actionTitle: "확인")
+            }
+        }
+        
+        let library = UIAlertAction(title: "앨범", style: .default) {_ in
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                presentLibrary()
+            } else {
+                self.presentOneButtonAlert(alertTitle: "알림",
+                                           message: "앨범 사용이 불가능합니다.",
+                                           actionTitle: "확인")
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        func verifyUsingCamera() -> Bool {
+            UIImagePickerController.isSourceTypeAvailable(.camera)
+        }
+        
+        func presentCamera() {
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        func presentLibrary() {
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        alert.addAction(camera)
+        alert.addAction(library)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
         
         if let formattedWalkingTime = timerStringFormatter.string(from: Double(walkRecordTime)) {
             walkRecordTimeLabel.text = "산책 시간: \(formattedWalkingTime)"
@@ -49,3 +103,15 @@ class WalkRecordEditingViewController: UIViewController {
     }
 }
 
+
+extension WalkRecordEditingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        walkRecordImageView.image = image
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
