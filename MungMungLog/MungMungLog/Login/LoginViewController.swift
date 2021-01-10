124dd31a99922bffada7bf2d8fa5da39e64872be
JOJUNHEE <eh7457303j@gmail.com>
2021-01-10 22:28:55 +0900

    로그인 화면 로고가 내려오는 문제 해결
    frame을 설정하는 코드 삭제.
    LogoImageView의 centerYConstraint를  logoCenterYConstraint로 outlet연결.
    logoCenterYConstraint을 이용해서 로고를 옮기도록 moveLogoToTop 메서드  수정.

--- MungMungLog/MungMungLog/Login/LoginViewController.swift
+++ MungMungLog/MungMungLog/Login/LoginViewController.swift
@@ -9,50 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var logoCenterYConstraint: NSLayoutConstraint!
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var loginStackView: UIStackView!
     @IBOutlet weak var passwordFindingView: UIView!
     
-    var logoImageViewOrigin: CGPoint {
-        get {
-            return logoImageView.frame.origin
-        } set {
-            logoImageView.frame.origin = newValue
-        }
-    }
-    
-    override func viewDidLayoutSubviews() {
-
-
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loginStackView.alpha = 0
         passwordFindingView.alpha = 0
-
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            moveLogoToTop()
+            
             UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseIn, animations: {
-                moveLogoToTop()
+                self.view.layoutIfNeeded()
                 presentLoginView()
             })
         }
         
         func moveLogoToTop() {
-            let logoFrame = logoImageView.frame
-            let logoCenterXPosition = view.center.x - (logoFrame.width / 2)
-            let logoCenterYPosition = view.center.y - (logoFrame.height / 2)
-            
-            logoImageViewOrigin = CGPoint(x: logoCenterXPosition,
-                                          y: logoCenterYPosition - (view.frame.height / 5))
+            self.logoCenterYConstraint.constant = -(self.view.frame.height / 5)
         }
         
         func presentLoginView() {
-            loginStackView.alpha = 1.0
-            passwordFindingView.alpha = 1.0
+            self.loginStackView.alpha = 1.0
+            self.passwordFindingView.alpha = 1.0
         }
+        
     }
     
 }
