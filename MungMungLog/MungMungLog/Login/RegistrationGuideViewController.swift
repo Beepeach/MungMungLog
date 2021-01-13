62c632b119e3c78c34263011bade235a38a5bc13
JOJUNHEE <eh7457303j@gmail.com>
2021-01-14 01:19:05 +0900

    등록가이드 화면 구성
    기본 이미지 사진인 enroll asset에 추가.

---
+++ MungMungLog/MungMungLog/Login/RegistrationGuideViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationGuideViewController.swift
+//  MungMungLog
+//
+//  Created by JunHee Jo on 2021/01/14.
+//
+
+import UIKit
+
+class RegistrationGuideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
