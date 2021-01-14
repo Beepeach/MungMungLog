fb915f25720149b24b1f040e197831e7e312f9c2
JOJUNHEE <eh7457303j@gmail.com>
2021-01-14 18:14:00 +0900

    프로필 등록화면 성별 선택 구현.
    버튼 UI 성별이미지 추가.
    male, female png asset에 등록.
    성별 선택 터치이벤트을 위해 action으로 selectMale, selectFemale 연결.
    선택한 성별을 구별할 수 있도록 isMale 속성 추가.

--- MungMungLog/MungMungLog/Login/EditingProfileViewController.swift
+++ MungMungLog/MungMungLog/Login/EditingProfileViewController.swift
@@ -8,8 +8,32 @@
 import UIKit
 
 class EditingProfileViewController: UIViewController {
+    
+    var isMale: Bool = true
+    
     @IBOutlet weak var maleContainerView: RoundedView!
     @IBOutlet weak var femaleContainerView: RoundedView!
+    
+    
+    @IBAction func selectMale(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+            maleContainerView.backgroundColor = .systemGray4
+        } else {
+            maleContainerView.backgroundColor = UIColor.lightGray
+        }
+        femaleContainerView.backgroundColor = .none
+        isMale = true
+    }
+    
+    @IBAction func selectFemale(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+            femaleContainerView.backgroundColor = .systemGray4
+        } else {
+            femaleContainerView.backgroundColor = UIColor.lightGray
+        }
+        maleContainerView.backgroundColor = .none
+        isMale = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
