3b8abb2b403bb895c6257e2b66a8a52fc943d3d5
JOJUNHEE <eh7457303j@gmail.com>
2021-02-06 22:52:32 +0900

    CPU 100%문제 재발생으로 ID, Password 입력안내문구 삭제
    똑같이 CPU100%문제로 ID, Password 입력 안내문구 삭제

--- MungMungLog/MungMungLog/Login/LoginViewController.swift
+++ MungMungLog/MungMungLog/Login/LoginViewController.swift
@@ -15,9 +15,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginScrollView: UIScrollView!
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var loginStackView: UIStackView!
-    @IBOutlet weak var incorrectIdFormatLabel: UILabel!
     @IBOutlet weak var idInputField: UITextField!
-    @IBOutlet weak var incorrectPasswordFormatLabel: UILabel!
     @IBOutlet weak var passwordInputField: UITextField!
     @IBOutlet weak var loginContainerView: RoundedView!
     @IBOutlet weak var passwordFindingView: UIView!
@@ -32,8 +30,6 @@ class LoginViewController: UIViewController {
         loginStackView.alpha = 0
         passwordFindingView.alpha = 0
         loginWithSnsStackView.alpha = 0
-        incorrectIdFormatLabel.alpha = 0
-        incorrectPasswordFormatLabel.alpha = 0
     }
     
     func setScreenWhenShowKeyboard() {
@@ -158,11 +154,9 @@ extension LoginViewController: UITextFieldDelegate {
                   range.lowerBound == finalText.startIndex && range.upperBound == finalText.endIndex else {
                 isAccessibleLoginId = false
                 checkLoginButtonEnable()
-                incorrectIdFormatLabel.alpha = 1
                 return true
             }
             
-            incorrectIdFormatLabel.alpha = 0
             isAccessibleLoginId = true
             checkLoginButtonEnable()
             return true
@@ -172,11 +166,9 @@ extension LoginViewController: UITextFieldDelegate {
                   finalText.count <= 20 else {
                 isAccessibleLoginPassword = false
                 checkLoginButtonEnable()
-                incorrectPasswordFormatLabel.alpha = 1
                 return true
             }
             
-            incorrectPasswordFormatLabel.alpha = 0
             isAccessibleLoginPassword = true
             checkLoginButtonEnable()
             return true
