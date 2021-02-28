6610b3ae3d51ca8d0deaee6110920e445b9deb11
JOJUNHEE <eh7457303j@gmail.com>
2021-02-28 19:28:18 +0900

    코드입력 화면 구현
    구성원으로 가입할때 필요한 코드를 입력하는 화면인 CodeInputViewController 생성.

---
+++ MungMungLog/MungMungLog/Login/CodeInputViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CodeInputViewController.swift
+//  MungMungLog
+//
+//  Created by JunHee Jo on 2021/02/28.
+//
+
+import UIKit
+
+class CodeInputViewController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
