9da82e7cca68110bc4cc85f754494197f20ee908
JOJUNHEE <eh7457303j@gmail.com>
2021-02-28 23:15:34 +0900

    코드입력화면 키보드 변화에 따른 button 위치 구현
    키보드가 나타나고 없어질때 버튼의 위치를 구현하기 위해 setButtonWhenShowKeyboard(), setButtonWhenHideKeyboard() 메서드 구현

--- MungMungLog/MungMungLog/Login/CodeInputViewController.swift
+++ MungMungLog/MungMungLog/Login/CodeInputViewController.swift
@@ -8,14 +8,52 @@
 import UIKit
 
 class CodeInputViewController: UIViewController {
-
+    
+    @IBOutlet weak var continueContainerViewBottomCostraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var ContinueContainerView: RoundedView!
+    
+    
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    @IBAction func enterCode(_ sender: Any) {
+        
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setButtonWhenShowKeyboard()
+        setButtonWhenHideKeyboard()
+    }
+    
+    func setButtonWhenShowKeyboard() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (noti) in
+            guard let userInfo = noti.userInfo else { return }
+            
+            guard let keyboardBound = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+            
+            self.continueContainerViewBottomCostraint.constant = keyboardBound.height + 10
+            
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    
+
+    func setButtonWhenHideKeyboard() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { (_) in
+            self.continueContainerViewBottomCostraint.constant = 36
+            
+            UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseIn) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
 }
+
+
+
