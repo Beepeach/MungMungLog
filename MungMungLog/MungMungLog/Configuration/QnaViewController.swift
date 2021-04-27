73d28220a99da93bdb94bd43349eaebd3eb8205c
JOJUNHEE <eh7457303j@gmail.com>
2021-04-28 01:38:36 +0900

    문의하기 화면 구성
    문의하기 상세화면 구성.

--- MungMungLog/MungMungLog/Configuration/QnaViewController.swift
+++ MungMungLog/MungMungLog/Configuration/QnaViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class QnaViewController: UIViewController {
 
+    @IBOutlet weak var titleField: UITextField!
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    
     @IBAction func backMyPageVC(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func send(_ sender: Any) {
+        presentOneButtonAlert(alertTitle: "알림", message: "문의하신 내용이 잘 전달되었습니다.", actionTitle: "확인") { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+    }
+}
+
+
+extension QnaViewController: UITextViewDelegate {
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        textView.text = ""
+        return true
     }
 }
