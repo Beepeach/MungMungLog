ea1c5ede74bd8b544c5dd1da9294f3f73de068af
Beepeach <eh7457303j@gmail.com>
2021-12-19 00:26:57 +0900

    Fix: 이동 Info.plist delegate storyboard등 기본 생성파일 위치 변경
    AppDelegate와 같은 기본 파일의 위치가 애매하여 predefined 디렉토리에
    모두 이동

--- MungMungLog/MungMungLog/Predefined/AppDelegate.swift
+++ MungMungLog/MungMungLog/Predefined/AppDelegate.swift
@@ -15,11 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
         KakaoSDKCommon.initSDK(appKey: "c21b7a3b3d287e24904b0f47c9c233f5")
         
         checkIsFirstlaunch()
         
         CoreDataManager.shared.setup(modelName: "MungMungLog")
+        
         
         return true
     }
@@ -34,6 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
+    
     // Kakao login
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
          if (AuthApi.isKakaoTalkLoginUrl(url)) {
@@ -43,17 +47,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          return false
      }
     
+    
 
-    // MARK: - UISceneSession Lifecycle
+    // MARK: UISceneSession Lifecycle
 
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+
+
 }
 
