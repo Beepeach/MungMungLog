8f967d1c5dee2c2869def5319c1f6b7c0f190bde
Beepeach <eh7457303j@gmail.com>
2021-12-18 22:17:07 +0900

    Fix: 수정 info.plist 경로 문제 및 pod 재설치
    info.plist의 경로문제가 발생하여 다시 추가 하고 pod file이 꼬여서 재설치

---
+++ MungMungLog/MungMungLog/Predefined/AppDelegate.swift
@@ -0,0 +1,59 @@
+//
+//  AppDelegate.swift
+//  MungMungLog
+//
+//  Created by JunHee Jo on 2020/11/08.
+//
+
+import UIKit
+import KakaoSDKCommon
+import KakaoSDKAuth
+import SwiftKeychainWrapper
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        KakaoSDKCommon.initSDK(appKey: "c21b7a3b3d287e24904b0f47c9c233f5")
+        
+        checkIsFirstlaunch()
+        
+        CoreDataManager.shared.setup(modelName: "MungMungLog")
+        
+        return true
+    }
+    
+    func checkIsFirstlaunch() {
+        if !UserDefaults.standard.bool(forKey: UserDefaultKeys.isSecondLaunch.rawValue) {
+            deleteKeychainInfo()
+            
+            UserDefaults.standard.set(true, forKey: UserDefaultKeys.isSecondLaunch.rawValue)
+        } else {
+            print("첫 실행이 아닙니다.")
+        }
+    }
+    
+    // Kakao login
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+         if (AuthApi.isKakaoTalkLoginUrl(url)) {
+             return AuthController.handleOpenUrl(url: url)
+         }
+
+         return false
+     }
+    
+
+    // MARK: - UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+}
+
---
+++ MungMungLog/MungMungLog/Predefined/SceneDelegate.swift
@@ -0,0 +1,56 @@
+//
+//  SceneDelegate.swift
+//  MungMungLog
+//
+//  Created by JunHee Jo on 2020/11/08.
+//
+
+import UIKit
+import KakaoSDKAuth
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    // KaKaoLogin
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
+    }
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+  
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        guard let sceneEnterBackgroundTime = UserDefaults.standard.object(forKey: "sceneDidEnterBackground") as? Date else {
+            return
+        }
+        
+        let elaspedTime: Double = Date().timeIntervalSince(sceneEnterBackgroundTime)
+        
+        NotificationCenter.default.post(name: .sceneWillEnterForeground, object: nil, userInfo: ["elaspedTime": elaspedTime])
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        NotificationCenter.default.post(name: .sceneDidEnterBackground, object: nil)
+        UserDefaults.standard.setValue(Date(), forKey: "sceneDidEnterBackground")
+    }
+}
+
