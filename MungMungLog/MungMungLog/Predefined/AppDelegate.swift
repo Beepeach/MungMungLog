d528d9a55bce0d4261ed00601a0846631d214317
Beepeach <eh7457303j@gmail.com>
2021-12-21 16:28:31 +0900

    Refact: 정리 App, SceneDelegate 코드 정리
    불필요한 공백과 주석 제거

--- MungMungLog/MungMungLog/Predefined/AppDelegate.swift
+++ MungMungLog/MungMungLog/Predefined/AppDelegate.swift
@@ -15,14 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
         KakaoSDKCommon.initSDK(appKey: "c21b7a3b3d287e24904b0f47c9c233f5")
         
         checkIsFirstlaunch()
         
         CoreDataManager.shared.setup(modelName: "MungMungLog")
-        
         
         return true
     }
@@ -37,7 +34,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
-    
     // Kakao login
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
          if (AuthApi.isKakaoTalkLoginUrl(url)) {
@@ -48,23 +44,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      }
     
     
-
     // MARK: UISceneSession Lifecycle
 
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
