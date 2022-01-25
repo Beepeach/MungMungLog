943122d6ef144891da1f03e2f1de1eaf426984fa
Beepeach <eh7457303j@gmail.com>
2022-01-25 17:52:39 +0900

    Refactor: CoreDataManager에서 container 생성코드 변경
    iOS 13.0 이전버전에서 발생하던 storeLoad문제가 더 이상 발생하지
    않기때문에 관련 코드를 삭제했습니다.

--- MungMungLog/MungMungLog/Data/CoreDataManager.swift
+++ MungMungLog/MungMungLog/Data/CoreDataManager.swift
@@ -31,28 +31,10 @@ class  CoreDataManager {
     }
     
     func setup(modelName: String) {
-//        container = NSPersistentContainer(name: modelName)
-        
-        if #available(iOS 13.0, *) {
-            container = NSPersistentContainer(name: modelName)
-        } else {
-            // 경고가 발생해서 추가한 코드
-            // 아직 이해가 되지않는 코드..
-            var modelURL = Bundle(for: type(of: self)).url(forResource: modelName, withExtension: "momd")!
-            let versionInfoURL = modelURL.appendingPathComponent("VersionInfo.plist")
-            if let versionInfoNSDictionary = NSDictionary(contentsOf: versionInfoURL),
-                let version = versionInfoNSDictionary.object(forKey: "NSManagedObjectModel_CurrentVersionName") as? String {
-                modelURL.appendPathComponent("\(version).mom")
-                let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)
-                container = NSPersistentContainer(name: modelName, managedObjectModel: managedObjectModel!)
-            } else {
-                //fall back solution; runs fine despite "Failed to load optimized model" warning
-                container = NSPersistentContainer(name: modelName)
-            }
-        }
-        
+        container = NSPersistentContainer(name: modelName)
         container?.loadPersistentStores(completionHandler: { (description, error) in
             if let error = error {
+                // TODO: Store 로드에러가 발생하면 어떻게 처리할까 고민해보자.
                 print(error)
             }
         })
